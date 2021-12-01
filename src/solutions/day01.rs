cc8c246a9d3720b0d1a5540bf3a993b593db12c2
sebous <sebous@gmail.com>
2021-12-01 09:47:37 +0100

    day 1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,11 +1,52 @@
-use crate::lib::input;
+use crate::lib::input::load_input;
+
+fn parse_input() -> Vec<usize> {
+    load_input("01")
+        .lines()
+        .map(|f| f.parse().unwrap())
+        .collect()
+}
+
+fn part_one(input: &Vec<usize>) -> usize {
+    let mut counter = 0;
+    for (i, item) in input.iter().enumerate() {
+        if i > 0 && item > &input[i - 1] {
+            counter += 1;
+        }
+    }
+    counter
+}
+
+fn part_two(input: &Vec<usize>) -> usize {
+    let mut counter = 0;
+    for i in 0..input.len() {
+        if i == 0 || input.len() <= i + 2 {
+            continue;
+        }
+
+        let a = input[i - 1] + input[i] + input[i + 1];
+        let b = input[i] + input[i + 1] + input[i + 2];
+
+        if b > a {
+            counter += 1;
+        }
+    }
+    counter
+}
 
 pub fn run() {
-    let input = input::load_input("01");
-    println!("{}", input);
+    let input = parse_input();
+
+    let p1 = part_one(&input);
+    println!("part 1: {}", p1);
+
+    let p2 = part_two(&input);
+    println!("part 2: {}", p2);
 }
 
 #[test]
-fn it_works() {
-    assert_eq!(2 + 2, 4);
+fn part_two_test() {
+    let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    let result = part_two(&input);
+    assert_eq!(result, 5);
 }
