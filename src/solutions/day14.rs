f0c9f12f4a7e56d494bebe5d7cabd69eea946bd6
sebous <sebous@gmail.com>
2021-12-14 13:24:27 +0100

    day 14 p2 btree solution

--- src/solutions/day14.rs
+++ src/solutions/day14.rs
@@ -1,99 +1,74 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 use itertools::Itertools;
 
 use crate::lib::input::load_input;
 
-type Rules = HashMap<String, char>;
+type Template = BTreeMap<(char, char), usize>;
+type Rules = BTreeMap<(char, char), char>;
 
-fn parse() -> (Vec<char>, HashMap<String, char>) {
+fn parse() -> (Template, Rules) {
     let input = load_input("14");
     let mut lines = input.lines();
-    let template = lines.next().clone().unwrap().chars().collect_vec();
+    let first_line = lines.next().unwrap().chars().collect_vec();
 
-    let rules: HashMap<String, char> = lines
-        .skip(1)
-        .map(|l| {
-            let (from, to) = l.split_once(" -> ").unwrap();
-            (from.to_string(), to.chars().next().unwrap())
-        })
+    let template = (0..first_line.len() - 1)
+        .map(|i| ((first_line[i], first_line[i + 1]), 1usize))
         .collect();
 
+    let rules = lines
+        .skip(1)
+        .map(|line| {
+            let mut line = line.chars();
+
+            (
+                (line.next().unwrap(), line.next().unwrap()),
+                line.last().unwrap(),
+            )
+        })
+        .collect();
     (template, rules)
 }
 
-fn polymerize(input: &Vec<char>, rules: &Rules) -> Vec<char> {
-    let mut result = vec![];
-    for i in 0..input.len() - 1 {
-        result.push(input[i]);
-        let pattern = format!("{}{}", input[i], input[i + 1]);
-        if let Some(rule) = rules.get(&pattern) {
-            result.push(*rule);
+fn polymerize(template: &Template, rules: &Rules) -> Template {
+    let mut result = BTreeMap::new();
+
+    for (&pair @ (a, b), n) in template {
+        if let Some(&c) = rules.get(&pair) {
+            *result.entry((a, c)).or_insert(0) += n;
+            *result.entry((c, b)).or_insert(0) += n;
+        } else {
+            *result.entry(pair).or_insert(0) += n;
         }
     }
-    result.push(input[input.len() - 1]);
     result
 }
 
-fn polymerize_recur(
-    input: &Vec<char>,
-    rules: &Rules,
-    buffer: &mut HashMap<char, usize>,
-    curr_depth: usize,
-    max_depth: usize,
-) {
-    if curr_depth == max_depth {
-        // println!("{}", curr_depth);
-        input.iter().for_each(|ch| {
-            *buffer.entry(*ch).or_insert(1) += 1;
-        });
-        return;
-    } else {
-        let mut new_polymer = input.clone();
-        for i in 0..input.len() - 1 {
-            new_polymer.push(input[i]);
-            let pattern = format!("{}{}", input[i], input[i + 1]);
-            if let Some(rule) = rules.get(&pattern) {
-                new_polymer.push(*rule);
-            }
-        }
-        new_polymer.push(*input.last().unwrap());
-
-        for i in 0..new_polymer.len() - 1 {
-            let pattern = format!("{}{}", new_polymer[i], new_polymer[i + 1])
-                .chars()
-                .collect_vec();
-            polymerize_recur(&pattern, rules, buffer, curr_depth + 1, max_depth);
-        }
+fn count_difference(template: &Template) -> usize {
+    let mut items = BTreeMap::new();
+    for (&(a, b), &count) in template {
+        *items.entry(a).or_insert(0) += count;
+        *items.entry(b).or_insert(0) += count;
     }
+    (items.values().max().unwrap() + 1) / 2 - (items.values().min().unwrap() + 1) / 2
 }
 
-fn part_one(template: &Vec<char>, rules: &Rules) {
-    let mut result = template.clone();
-    for i in 0..10 {
-        println!("{}", i);
-        result = polymerize(&result, rules);
-    }
-    let results_map = result.iter().fold(HashMap::new(), |mut acc, curr| {
-        *acc.entry(*curr).or_insert(1) += 1;
-        acc
-    });
-    let mut results_sorted = results_map.iter().sorted_by(|a, b| b.1.cmp(a.1));
-    println!(
-        "{}",
-        results_sorted.next().unwrap().1 - results_sorted.last().unwrap().1
-    );
+fn part_one() -> usize {
+    let (template, rules) = parse();
+    let result = (0..2).fold(template, |acc, _| polymerize(&acc, &rules));
+    count_difference(&result)
 }
 
-fn part_two(template: &Vec<char>, rules: &Rules) {
-    let mut buffer = HashMap::new();
-    polymerize_recur(template, rules, &mut buffer, 0, 5);
-
-    println!("{:?}", buffer);
+fn part_two() -> usize {
+    let (template, rules) = parse();
+    let result = (0..40).fold(template, |acc, _| polymerize(&acc, &rules));
+    count_difference(&result)
 }
 
 pub fn run() {
-    let (template, rules) = parse();
-    part_one(&template, &rules);
-    // part_two(&template, &rules);
+    let p1 = part_one();
+    println!("part 1: {}", p1);
+
+    let p2 = part_two();
+    println!("part 2: {}", p2);
 }
